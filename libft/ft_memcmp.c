d378aa93e8ab61b9429daf5cffad233690210fcc
Kilyan Poilly <kpoilly@made-f0Cr7s2.clusters.42paris.fr>
2023-11-10 18:16:32 +0100

    memcmp ok

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 14:18:41 by kpoilly           #+#    #+#             */
-/*   Updated: 2023/11/01 14:21:05 by kpoilly          ###   ########.fr       */
+/*   Updated: 2023/11/10 18:13:24 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	return (ft_strncmp((char *)s1, (char *)s2, n));
+	size_t	i;
+
+	if (!n)
+		return (0);
+	i = 0;
+	while ((((unsigned char *)s1)[i] == ((unsigned char *)s2)[i])
+			&& i < n - 1)
+		i++;
+	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 }
