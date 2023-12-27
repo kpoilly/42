925921925fd95a6109b404ac2b6eac1c31e91964
Kilyan Poilly <kpoilly@paul-f4Ar8s7.clusters.42paris.fr>
2023-12-27 11:16:33 +0100

    check error

--- so_long/check_error.c
+++ so_long/check_error.c
@@ -6,47 +6,76 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 15:12:15 by kpoilly           #+#    #+#             */
-/*   Updated: 2023/12/19 08:48:35 by kpoilly          ###   ########.fr       */
+/*   Updated: 2023/12/27 11:15:53 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./data/headers/so_long.h"
 
-//Compte le nombre de collectibles dans la map
-int	count_collec(char	**map, t_global *global, int *nb_exit, int *nb_player)
+int	in_charset(char c, char *charset)
 {
-	*nb_exit = 0;
-	*nb_player = 0;
-	global->nbcollec = 0;
-	(void)map;
-	//return 0 si !in_charset("01CEP")
+	while (*charset)
+	{
+		if (c == *charset)
+			return (1);
+		charset++;
+	}
+	return (0);
+}
+
+// Compte le nombre de collectibles dans la map
+int	count_collec(char **map, t_global *global, int *nb_exit, int *nb_player)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	while (map[y])
+	{
+		x = 0;
+		while (map[y][x])
+		{
+			if (!in_charset(map[y][x], "01CEP"))
+				return (ft_printf("Error.\n"),
+					ft_printf("An unrecognized character is Among Us.\n"), 0);
+			if (map[y][x] == 'C')
+				global->nbcollec++;
+			if (map[y][x] == 'E')
+				(*nb_exit)++;
+			if (map[y][x] == 'P')
+				(*nb_player)++;
+			x++;
+		}
+		if (x != ft_strlen(map[0]))
+			return (ft_printf("Error.\nDifferent line size."), 0);
+		y++;
+	}
 	return (1);
 }
 
-//Check les différentes erreurs de map
+// Check les différentes erreurs de map
 char	**check_error(char **map, t_global *global)
-{	
-	//int	nb_exit;
-	//int	nb_player;
-	(void)global;
+{
+	int	nb_exit;
+	int	nb_player;
+
+	nb_exit = 0;
+	nb_player = 0;
+	global->nbcollec = 0;
 	if (!map)
 		return (ft_printf("Error.\nMap is not readable.\n"), NULL);
-	// count_collec(map, global, &nb_exit, &nb_player);
-	// if (!global->nbcollec)
-	// 	return (ft_printf("Error.\nToo few collectibles.\n"), NULL);
-	// if (!nb_exit)
-	// 	return (ft_printf("Error.\nNo exit on the map.\n"), NULL);
-	// if (!nb_player)
-	// 	return (ft_printf("Error.\nNo Player on the map.\n"), NULL);
-	// if (nb_exit > 1)
-	// 	return (ft_printf("Error.\nToo many exit on the map.\n"), NULL);
-	// if (nb_player > 1)
-	// 	return (ft_printf("Error.\nThe game is not multiplayer !\n"), NULL);
-	//check les erreurs suivantes :
-	// - meme strlen sur chaque ligne
-	// - ne contient rien d'autres que le charset "01CEP"
-	// - contient au moins 1 : E, C et P
-	// - ne contient pas plusieurs : E et P
+	if (!count_collec(map, global, &nb_exit, &nb_player))
+		return (NULL);
+	if (!global->nbcollec)
+		return (ft_printf("Error.\nToo few collectibles.\n"), NULL);
+	if (!nb_exit)
+		return (ft_printf("Error.\nNo exit on the map.\n"), NULL);
+	if (!nb_player)
+		return (ft_printf("Error.\nNo Player on the map.\n"), NULL);
+	if (nb_exit > 1)
+		return (ft_printf("Error.\nToo many exit on the map.\n"), NULL);
+	if (nb_player > 1)
+		return (ft_printf("Error.\nThe game is not multiplayer !\n"), NULL);
 	// - map entouree de 1
 	// - check si tous les collectibles sont accessibles
 	return (map);
