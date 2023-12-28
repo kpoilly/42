8a3e45e7a0709c4f6fe06910dc55def16f96b14e
Kilyan Poilly <kpoilly@paul-f4Ar8s7.clusters.42paris.fr>
2023-12-28 21:17:23 +0100

    leaks--

--- so_long/get_map.c
+++ so_long/get_map.c
@@ -3,14 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   get_map.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 15:08:51 by kpoilly           #+#    #+#             */
-/*   Updated: 2023/12/17 14:19:08 by marvin           ###   ########.fr       */
+/*   Updated: 2023/12/28 21:16:37 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./data/headers/so_long.h"
+
+static int	maplen(char *filename)
+{
+	char	*str;
+	int		fd;
+	int		i;
+
+	i = 0;
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	str = get_next_line(fd);
+	while (str)
+	{
+		free(str);
+		i++;
+		str = get_next_line(fd);
+	}
+	close (fd);
+	return (i);
+}
 
 //Recupere la map dans un fichier .ber et la met dans un char**
 char	**get_map(char *filename, int *nb_line, int *nb_col)
@@ -19,13 +40,9 @@ char	**get_map(char *filename, int *nb_line, int *nb_col)
 	int		i;
 	char	**map;
 
-	i = 0;
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	i = maplen(filename);
+	if (!i)
 		return (NULL);
-	while (get_next_line(fd))
-		i++;
-	close (fd);
 	map = malloc((i + 1) * sizeof(char *));
 	*nb_line = i;
 	i = 0;
