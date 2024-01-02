49342c200e093a29e1a3a4ccc0324b140226526f
Kilyan Poilly <kpoilly@paul-f4Ar4s1.clusters.42paris.fr>
2024-01-02 14:55:51 +0100

    oopsie

--- so_long/ennemy_move.c
+++ so_long/ennemy_move.c
@@ -6,7 +6,7 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 12:50:18 by kpoilly           #+#    #+#             */
-/*   Updated: 2023/12/28 20:06:33 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/01/02 14:55:09 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	track_player(t_global *global, int x, int y)
 		return (move_ennemy_tab(global, &(global->map[y][x - 1]),
 			&(global->map[y][x])));
 	else if (global->player.x > x && global->map[y][x + 1]
-		&& (global->map[y][x + 1] != '0' || global->map[y][x + 1] != 'P'))
+		&& (global->map[y][x + 1] == '0' || global->map[y][x + 1] == 'P'))
 		return (move_ennemy_tab(global, &(global->map[y][x + 1]),
 			&(global->map[y][x])));
 	else if (global->player.y < y && y && (global->map[y - 1][x] == '0'
