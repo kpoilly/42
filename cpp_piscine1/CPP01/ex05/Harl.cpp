0d55719784dcfd208f246b1f7a0326c2df1ff8af
Kilyan Poilly <kpoilly@paul-f4Br2s1.clusters.42paris.fr>
2024-03-06 11:49:45 +0100

    ex05

--- cpp_piscine1/CPP01/ex05/Harl.cpp
+++ cpp_piscine1/CPP01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 09:54:20 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/03/06 10:45:05 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/03/06 11:48:50 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,17 @@ Harl::~Harl(){};
 
 void Harl::debug(void)
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-\
-	triple-pickle-special-ketchup burger. I really do!" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put \
-	enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for \
-	years whereas you started working here since last month." << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(void)
@@ -40,7 +37,7 @@ void Harl::error(void)
 void Harl::complain(std::string level)
 {
 	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	void (Harl::*funcs[])(void) = {Harl::debug, Harl::info, Harl::warning, Harl::error};
+	void (Harl::*funcs[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 	bool found = false;
 
 	for (int i = 0; i < level.size(); i++)
--- cpp_piscine1/CPP01/ex05/main.cpp
+++ cpp_piscine1/CPP01/ex05/main.cpp
@@ -6,13 +6,13 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 09:54:28 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/03/06 10:24:02 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/03/06 11:47:14 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int	main(int ac, char)
+int	main(int ac, char** av)
 {
 	Harl harl;
 
