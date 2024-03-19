461f3c876938e28df8897f3fa27f58bbd218538b
Kilyan Poilly <kpoilly@paul-f4Ar8s6.clusters.42paris.fr>
2024-03-19 07:34:15 +0100

    CPP00

--- cpp_piscine1/CPP00/ex00/megaphone.cpp
+++ cpp_piscine1/CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 08:59:00 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/01/23 09:55:05 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/03/19 07:31:07 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	main(int argc, char **argv)
 {
 	if (argc < 2 || !*(argv[1]))
 	{
-		std::cout << "* LOUD AND UNBEARBALE FEEDBACK NOISE *"<<std::endl;
+		std::cout << "* LOUD AND UNBEARBALE FEEDBACK NOISE *" << std::endl;
 		return (1);
 	}
 	for(int i = 1; i < argc; i++)
