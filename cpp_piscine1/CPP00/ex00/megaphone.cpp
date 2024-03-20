91ca9d1f5662d957047386ca8813dd2ec6d9ad92
Kilyan Poilly <kpoilly@paul-f4Ar8s6.clusters.42paris.fr>
2024-03-20 09:59:12 +0100

    CPP00

--- cpp_piscine1/CPP00/ex00/megaphone.cpp
+++ cpp_piscine1/CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 08:59:00 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/03/19 07:31:07 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/03/20 09:58:43 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,30 @@
 void	megaphone(std::string str)
 {
 	for(int i = 0; i < str.size(); i++)
-	{
-		if (islower(str[i]))
 			std::cout << (char)toupper(str[i]);
-		else
-			std::cout << (char)str[i];
-	}
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc < 2 || !*(argv[1]))
+	if (argc < 2)
 	{
 		std::cout << "* LOUD AND UNBEARBALE FEEDBACK NOISE *" << std::endl;
 		return (1);
 	}
+	
+	std::string str = argv[1];
+	if (str.find_first_not_of(" \t\v\n") && argc == 2)
+	{
+		std::cout << "* LOUD AND UNBEARBALE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+	
 	for(int i = 1; i < argc; i++)
+	{
 		megaphone(argv[i]);
+		if (i < argc - 1)
+			std::cout << " ";
+	}
 	std::cout<<std::endl;
 	return (0);
 }
