365cdada36054e029beb7f36c2d0bbab69762027
Kilyan Poilly <kpoilly@paul-f4Br3s3.clusters.42paris.fr>
2024-05-03 07:42:05 +0200

    CPP 01 RtP

--- cpp_piscine1/CPP01/ex01/main.cpp
+++ cpp_piscine1/CPP01/ex01/main.cpp
@@ -6,19 +6,16 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 10:42:47 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/05/02 13:29:41 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/05/03 07:41:38 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-int	main(int ac, char **av)
+int	main(void)
 {
-	if (ac != 3)
-		return (1);
-	
-	int	N = atoi(av[1]);
-	Zombie *horde = zombieHorde(N, av[2]);
+	int	N = 7;
+	Zombie *horde = zombieHorde(N, "Paul");
 	
 	for (int i = 0; i < N; i++)
 		horde[i].announce();
