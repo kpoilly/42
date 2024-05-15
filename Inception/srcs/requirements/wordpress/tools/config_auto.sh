e066c3d85d25eae62d6a0bd9160732f6e1ed94c6
Kilyan Poilly <kpoilly@paul-f4Br3s3.clusters.42paris.fr>
2024-05-15 11:30:06 +0200

    Incepcep

---
+++ Inception/srcs/requirements/wordpress/tools/config_auto.sh
@@ -0,0 +1,22 @@
+sleep 10
+
+if test -f "/var/www/html/wp-config.php";
+then
+	echo "wordpress already set"
+else
+	wp config create --allow-root\
+					--dbname=$SQL_DATABASE \
+					--dbuser=$SQL_USER \
+					--dbpass=$SQL_PASSWORD \
+					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+	# wp core install --allow-root
+
+	# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	# cp wp-config-sample.php wp-config.php
+fi
+
+exec "$@"
