57f9c9be0db95e133e66d20e491cc0313d2c36b8
kpoilly <kpoilly@kpoilly.42paris.fr>
2024-05-20 05:01:19 -0400

    INCEPCEP

--- Inception/srcs/requirements/wordpress/tools/config_auto.sh
+++ Inception/srcs/requirements/wordpress/tools/config_auto.sh
@@ -12,17 +12,9 @@ else
 					 --dbuser=$SQL_USER \
 					 --dbpass=$SQL_PASSWORD \
 					 --dbhost=mariadb:3306 --path='/var/www/wordpress'
-	wp core install --url=localhost --title=inception --admin-user=kpoilly \
+	wp core install --url=kpoilly.42.fr --title=inception --admin-user=kpoilly \
 					--admin_password=kpoilly42 --admin_email=kpoilly@student.42.fr \
 					--allow-root
-
-	# wp core install --allow-root
-
-	# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	# cp wp-config-sample.php wp-config.php
 fi
 
 exec "$@"
