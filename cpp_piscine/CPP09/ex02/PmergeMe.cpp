2a72a41807beb3018a635afda148a6943ab2e1fb
Kilyan Poilly <kpoilly@paul-f4Br3s2.clusters.42paris.fr>
2024-06-14 13:52:43 +0200

    CPP09

--- cpp_piscine/CPP09/ex02/PmergeMe.cpp
+++ cpp_piscine/CPP09/ex02/PmergeMe.cpp
@@ -6,9 +6,20 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 13:07:51 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/06/13 13:08:08 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/06/14 13:51:19 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
+void	merginsSort(std::vector<int>& cont)
+{
+	(void)cont;
+	std::cout << "Vector sort !" << std::endl;
+};
+
+void	merginsSort(std::list<int>& cont)
+{
+	(void)cont;
+	std::cout << "List sort !" << std::endl;
+};
--- cpp_piscine/CPP09/ex02/main.cpp
+++ cpp_piscine/CPP09/ex02/main.cpp
@@ -6,12 +6,11 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 07:24:40 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/06/14 13:42:39 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/06/14 13:51:52 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
-#include <unistd.h>
 
 bool	validInput(std::string nb)
 {
@@ -51,22 +50,18 @@ int	main(int ac, char**av)
 		return 42;
 	}
 	
-	std::vector<int> cont1 = fillCont<std::vector<int> >(av[1]);
-	std::deque<int> cont2 = fillCont<std::deque<int> >(av[1]);
-	
-	std::cout << "Before: ";
-	for (unsigned long i = 0; i < cont1.size(); i++)
-		std::cout << cont1[i] << " ";
-	std::cout << std::endl;
+	std::cout << "Before: " << av[1] << std::endl;
 
 	struct timeval tv1, tv2;
 	gettimeofday(&tv1, NULL);
-	//merginsSort(cont1);
+	std::vector<int> cont1 = fillCont<std::vector<int> >(av[1]);
+	merginsSort(cont1);
 	gettimeofday(&tv2, NULL);
 	double time1 = (double) (tv2.tv_usec - tv1.tv_usec);
 
 	gettimeofday(&tv1, NULL);
-	//merginsSort(cont2);
+	std::list<int> cont2 = fillCont<std::list<int> >(av[1]);
+	merginsSort(cont2);
 	gettimeofday(&tv2, NULL);
 	double time2 = (double) (tv2.tv_usec - tv1.tv_usec);
 
@@ -79,6 +74,6 @@ int	main(int ac, char**av)
 	std::cout << " with std::" << "vector" << " :  " << time1 << " us" << std::endl;
 
 	std::cout << "Time to process a range of " << cont2.size() << " elements";
-	std::cout << " with std::" << "deque" << "  :  " << time2 << " us" << std::endl;
+	std::cout << " with std::" << "list" << "   :  " << time2 << " us" << std::endl;
 	return 0;
 };
