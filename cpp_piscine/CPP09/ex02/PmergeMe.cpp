23aaa2f95a977879e435c16888a53e3c219477b7
Kilyan Poilly <kpoilly@paul-f4Ar7s11.clusters.42paris.fr>
2024-06-17 15:41:42 +0200

    CPP09

--- cpp_piscine/CPP09/ex02/PmergeMe.cpp
+++ cpp_piscine/CPP09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 13:07:51 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/06/17 14:50:59 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/06/17 15:40:58 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 bool	sortCondition(const std::pair<int, int>& a, const std::pair<int, int>& b)
 {
-	return a.first < b.first;
+	return a.second < b.second;
 }
 
 void	merginsSort(std::vector<int>& cont)
@@ -33,25 +33,25 @@ void	merginsSort(std::vector<int>& cont)
 	
 	//Sort pairs by a
 	std::sort(pairs.begin(), pairs.end(), sortCondition);
-	//If the nb of nums is odd, the unpaired element is added to the back
-	if (cont.size() % 2 == 1)
-		pairs.push_back(std::make_pair(-1, cont[cont.size()-1]));
+	// //If the nb of nums is odd, the unpaired element is added to the back
+	// if (cont.size() % 2 == 1)
+	// 	pairs.push_back(std::make_pair(-1, cont[cont.size()-1]));
 	
 	//Add every a to the final sorted vector
 	cont.clear();
 	for (unsigned long i = 0; i < pairs.size(); i++)
-		if (pairs[i].first >= 0)
-			cont.push_back(pairs[i].first);
+		cont.push_back(pairs[i].second);
 
+	//INSERT
+	cont.insert(cont.begin(), pairs.front().first);
+	pairs.erase(pairs.begin());
+	
 	std::cout << "Pairs:" << std::endl;
 	for (unsigned long i = 0; i < pairs.size(); i++)
 		std::cout << i << ": " << pairs[i].first << " | " << pairs[i].second << std::endl;
 	std::cout << "sorted:" << std::endl;
 	for (unsigned long i = 0; i < cont.size(); i++)
 		std::cout << i << ": " << cont[i] << std::endl;
-
-	//INSERT
-	
 };
 
 void	merginsSort(std::list<int>& cont)
@@ -73,22 +73,23 @@ void	merginsSort(std::list<int>& cont)
 	
 	//Sort pairs by a
 	pairs.sort(sortCondition);
-	//If the nb of nums is odd, the unpaired element is added to the back
-	if (cont.size() % 2 == 1)
-		pairs.push_back(std::make_pair(-1, cont.back()));
+	// //If the nb of nums is odd, the unpaired element is added to the back
+	// if (cont.size() % 2 == 1)
+	// 	pairs.push_back(std::make_pair(-1, cont.back()));
 	
 	//Add every a to the final sorted vector
 	cont.clear();
 	for (std::list<std::pair<int, int> >::iterator it = pairs.begin(); it != pairs.end(); it++)
-		if ((*it).first >= 0)
-			cont.push_back((*it).first);
+			cont.push_back((*it).second);
 
+	//INSERT
+	cont.insert(cont.begin(), pairs.front().first);
+	pairs.erase(pairs.begin());
+	
 	std::cout << "Pairs:" << std::endl;
 	for (std::list<std::pair<int, int> >::iterator it = pairs.begin(); it != pairs.end(); it++)
 		std::cout << (*it).first << " | " << (*it).second << std::endl;
 	std::cout << "sorted:" << std::endl;
 	for (std::list<int>::iterator it = cont.begin(); it != cont.end(); it++)
 		std::cout << *it << std::endl;
-
-	//INSERT
 };
