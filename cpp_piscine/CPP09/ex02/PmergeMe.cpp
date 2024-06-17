d5eb39bdf0e089225e13017476a689cc295f5e55
Kilyan Poilly <kpoilly@paul-f4Ar7s11.clusters.42paris.fr>
2024-06-17 14:48:28 +0200

    CPP09

--- cpp_piscine/CPP09/ex02/PmergeMe.cpp
+++ cpp_piscine/CPP09/ex02/PmergeMe.cpp
@@ -6,20 +6,104 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 13:07:51 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/06/14 13:51:19 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/06/17 14:47:51 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
+bool	sortCondition(const std::pair<int, int>& a, const std::pair<int, int>& b)
+{
+	return a.first < b.first;
+}
+
 void	merginsSort(std::vector<int>& cont)
 {
-	(void)cont;
-	std::cout << "Vector sort !" << std::endl;
+	std::cout << "\nVECTOR:\n" << std::endl;
+	//MERGE
+	//Make Pairs
+	std::vector<std::pair<int, int> > pairs;
+	for (unsigned long i = 0; i < cont.size()-1; i+=2)
+	{
+		if (cont[i] < cont[i+1])
+			pairs.push_back(std::make_pair(cont[i], cont[i+1]));
+		else
+			pairs.push_back(std::make_pair(cont[i+1], cont[i]));
+	}
+	
+	//Sort pairs by a
+	std::sort(pairs.begin(), pairs.end(), sortCondition);
+	// for (unsigned long i = 0; i < pairs.size()-1; i++)
+	// {	
+	// 	if (pairs[i].first > pairs[i+1].first)
+	// 	{
+	// 		std::pair<int, int> tmp = pairs[i];
+	// 		pairs[i] = pairs[i+1];
+	// 		pairs[i+1] = tmp;
+	// 		i = 0;
+	// 	}
+	// }
+		
+	//If the nb of nums is odd, the unpaired element is added to the back
+	if (cont.size() % 2 == 1)
+		pairs.push_back(std::make_pair(-1, cont[cont.size()-1]));
+	
+	//Add every a to the final sorted vector
+	cont.clear();
+	for (unsigned long i = 0; i < pairs.size(); i++)
+	{
+		if (pairs[i].first >= 0)
+			cont.push_back(pairs[i].first);
+	}
+
+	std::cout << "Pairs:" << std::endl;
+	for (unsigned long i = 0; i < pairs.size(); i++)
+		std::cout << i << ": " << pairs[i].first << " | " << pairs[i].second << std::endl;
+	std::cout << "sorted:" << std::endl;
+	for (unsigned long i = 0; i < cont.size(); i++)
+		std::cout << i << ": " << cont[i] << std::endl;
+
+	//INSERT
+	
 };
 
 void	merginsSort(std::list<int>& cont)
 {
-	(void)cont;
-	std::cout << "List sort !" << std::endl;
+	std::cout << "\nLIST:\n" << std::endl;
+	//MERGE
+	//Make Pairs
+	std::list<std::pair<int, int> > pairs;
+	while(cont.size() > 1)
+	{
+		pairs.push_back(std::make_pair(cont.front(), cont.front()));
+		cont.pop_front();
+		if (cont.front() > pairs.back().first)
+			pairs.back().second = cont.front();
+		else
+			pairs.back().first = cont.front();
+		cont.pop_front();
+	}
+	
+	//Sort pairs by a
+	pairs.sort(sortCondition);
+	//If the nb of nums is odd, the unpaired element is added to the back
+	if (cont.size() % 2 == 1)
+		pairs.push_back(std::make_pair(-1, cont.back()));
+	
+	//Add every a to the final sorted vector
+	cont.clear();
+	for (std::list<std::pair<int, int> >::iterator it = pairs.begin(); it != pairs.end(); it++)
+	{
+		if ((*it).first >= 0)
+			cont.push_back((*it).first);
+	}
+
+	std::cout << "Pairs:" << std::endl;
+	for (std::list<std::pair<int, int> >::iterator it = pairs.begin(); it != pairs.end(); it++)
+		std::cout << (*it).first << " | " << (*it).second << std::endl;
+	std::cout << "sorted:" << std::endl;
+	for (std::list<int>::iterator it = cont.begin(); it != cont.end(); it++)
+		std::cout << *it << std::endl;
+
+	//INSERT
 };
--- cpp_piscine/CPP09/ex02/PmergeMe.hpp
+++ cpp_piscine/CPP09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 13:07:13 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/06/14 13:50:44 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/06/17 14:38:54 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <string>
 # include <vector>
 # include <list>
+# include <algorithm>
 
 void	merginsSort(std::vector<int>& cont);
 void	merginsSort(std::list<int>& cont);
