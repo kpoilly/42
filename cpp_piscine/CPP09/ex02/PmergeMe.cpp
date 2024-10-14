ed1d53fa32596abc25863c67c2e5dff538dfcffc
Kilyan Poilly <kpoilly@made-f0Cr13s4.clusters.42paris.fr>
2024-10-14 15:14:34 +0200

    coming soon

--- cpp_piscine/CPP09/ex02/PmergeMe.cpp
+++ cpp_piscine/CPP09/ex02/PmergeMe.cpp
@@ -12,14 +12,15 @@
 
 #include "PmergeMe.hpp"
 
-// Fonction pour calculer le n-ième terme de la suite de Jacobsthal
-int jacobsthal(int n) {
+int jacobsthal(int n) 
+{
     if (n == 0) return 0;
     if (n == 1) return 1;
     return jacobsthal(n-1) + 2*jacobsthal(n-2);
 }
 
-void insert(std::deque<int>& arr, int left, int right) {
+void insert(std::deque<int>& arr, int left, int right) 
+{
     for (int i = left + 1; i <= right; ++i) {
         int key = arr[i];
         int j = i - 1;
@@ -31,7 +32,8 @@ void insert(std::deque<int>& arr, int left, int right) {
     }
 }
 
-void insert(std::vector<int>& arr, int left, int right) {
+void insert(std::vector<int>& arr, int left, int right) 
+{
     for (int i = left + 1; i <= right; ++i) {
         int key = arr[i];
         int j = i - 1;
@@ -43,20 +45,18 @@ void insert(std::vector<int>& arr, int left, int right) {
     }
 }
 
-void merge(std::deque<int>& arr, int left, int mid, int right) {
+void merge(std::deque<int>& arr, int left, int mid, int right) 
+{
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
-    // Créer des tableaux temporaires
-    std::vector<int> L(n1), R(n2);
+    std::deque<int> L(n1), R(n2);
 
-    // Copier les données dans les tableaux temporaires L[] et R[]
     for (int i = 0; i < n1; i++)
         L[i] = arr[left + i];
     for (int j = 0; j < n2; j++)
         R[j] = arr[mid + 1 + j];
 
-    // Fusionner les tableaux temporaires en un seul
     int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
@@ -69,14 +69,12 @@ void merge(std::deque<int>& arr, int left, int mid, int right) {
         k++;
     }
 
-    // Copier les éléments restants de L[], s'il en reste
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    // Copier les éléments restants de R[], s'il en reste
     while (j < n2) {
         arr[k] = R[j];
         j++;
@@ -84,11 +82,12 @@ void merge(std::deque<int>& arr, int left, int mid, int right) {
     }
 }
 
-void merge(std::vector<int>& arr, int left, int mid, int right) {
+void merge(std::vector<int>& arr, int left, int mid, int right) 
+{
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
-    // Créer des tableaux temporaires
+    // tableaux temporaires
     std::vector<int> L(n1), R(n2);
 
     // Copier les données dans les tableaux temporaires L[] et R[]
@@ -99,11 +98,14 @@ void merge(std::vector<int>& arr, int left, int mid, int right) {
 
     // Fusionner les tableaux temporaires en un seul
     int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
+    while (i < n1 && j < n2) 
+    {
+        if (L[i] <= R[j]) 
+        {
             arr[k] = L[i];
             i++;
-        } else {
+        } else 
+        {
             arr[k] = R[j];
             j++;
         }
@@ -111,60 +113,68 @@ void merge(std::vector<int>& arr, int left, int mid, int right) {
     }
 
     // Copier les éléments restants de L[], s'il en reste
-    while (i < n1) {
+    while (i < n1) 
+    {
         arr[k] = L[i];
         i++;
         k++;
     }
 
     // Copier les éléments restants de R[], s'il en reste
-    while (j < n2) {
+    while (j < n2) 
+    {
         arr[k] = R[j];
         j++;
         k++;
     }
 }
 
-void slice(std::deque<int>& arr, int left, int right) {
-  if (right - left <= 1) {
-    return; // Un seul élément ou une paire, déjà triée
+void slice(std::deque<int>& arr, int left, int right) 
+{
+  if (right - left <= 1) 
+  {
+    return;
   }
 
   int mid = std::min(left + jacobsthal((right - left) / 2), right - 1);
   slice(arr, left, mid);
   slice(arr, mid + 1, right);
-  merge(arr, left, mid, right); // Call the merge function
+  merge(arr, left, mid, right);
 }
 
-void slice(std::vector<int>& arr, int left, int right) {
-  if (right - left <= 1) {
-    return; // Un seul élément ou une paire, déjà triée
+void slice(std::vector<int>& arr, int left, int right) 
+{
+  if (right - left <= 1) 
+  {
+    return; // Un seul élément ou déjà triée
   }
 
   int mid = std::min(left + jacobsthal((right - left) / 2), right - 1);
   slice(arr, left, mid);
   slice(arr, mid + 1, right);
-  merge(arr, left, mid, right); // Call the merge function
+  merge(arr, left, mid, right);
 }
 
-void sort(std::deque<int>& arr) {
+void sort(std::deque<int>& arr) 
+{
     int n = arr.size();
 
-    // Tri des paires
-    for (int i = 0; i < n - 1; i += 2) {
+    for (int i = 0; i < n - 1; i += 2) 
+    {
         insert(arr, i, std::min(i+1, n-1));
     }
-    // Appel récursif pour la fusion
 	slice(arr, 0, n-1);
 }
 
-void sort(std::vector<int>& arr) {
+void sort(std::vector<int>& arr) 
+{
     int n = arr.size();
 
     // Tri des paires
-    for (int i = 0; i < n - 1; i += 2) {
+    for (int i = 0; i < n - 1; i += 2) 
+    {
         insert(arr, i, std::min(i+1, n-1));
     }
-    // Appel récursif pour la fusion
+    // Merge recursif
 	slice(arr, 0, n-1);
 }
