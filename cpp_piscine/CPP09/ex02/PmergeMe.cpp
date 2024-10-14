0e409d50202098fdb34aa0a9de5061823a84bab2
Kilyan Poilly <kpoilly@made-f0Cr13s4.clusters.42paris.fr>
2024-10-14 15:31:56 +0200

    coming soon

--- cpp_piscine/CPP09/ex02/PmergeMe.cpp
+++ cpp_piscine/CPP09/ex02/PmergeMe.cpp
@@ -62,24 +62,30 @@ void merge(std::deque<int>& arr, int left, int mid, int right)
         R[j] = arr[mid + 1 + j];
 
     int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
+    while (i < n1 && j < n2) 
+    {
+        if (L[i] <= R[j]) 
+        {
             arr[k] = L[i];
             i++;
-        } else {
+        } 
+        else 
+        {
             arr[k] = R[j];
             j++;
         }
         k++;
     }
 
-    while (i < n1) {
+    while (i < n1) 
+    {
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    while (j < n2) {
+    while (j < n2) 
+    {
         arr[k] = R[j];
         j++;
         k++;
@@ -108,7 +114,8 @@ void merge(std::vector<int>& arr, int left, int mid, int right)
         {
             arr[k] = L[i];
             i++;
-        } else 
+        } 
+        else 
         {
             arr[k] = R[j];
             j++;
