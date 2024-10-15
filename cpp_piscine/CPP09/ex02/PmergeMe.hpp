38e0adad4ca8796d7ee3fe27268e98d2a1ede4f0
Kilyan Poilly <kpoilly@made-f0Cr13s4.clusters.42paris.fr>
2024-10-15 16:33:47 +0200

    coming soon

--- cpp_piscine/CPP09/ex02/PmergeMe.hpp
+++ cpp_piscine/CPP09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 13:07:13 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/10/14 13:10:33 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/10/15 16:28:03 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 # include <deque>
 # include <algorithm>
 
-void sort(std::vector<int>& arr);
-void sort(std::deque<int>& arr);
+void merge_insert_sort(std::vector<int>& vec, int left, int right);
+void merge_insert_sort(std::deque<int>& vec, int left, int right);
 
 #endif
--- cpp_piscine/CPP09/ex02/main.cpp
+++ cpp_piscine/CPP09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kpoilly <kpoilly@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 07:24:40 by kpoilly           #+#    #+#             */
-/*   Updated: 2024/10/14 13:27:21 by kpoilly          ###   ########.fr       */
+/*   Updated: 2024/10/15 16:33:23 by kpoilly          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ int	main(int ac, char**av)
 	gettimeofday(&tv1, NULL);
 	std::vector<int> cont1 = fillCont<std::vector<int> >(input);
 	std::cout << "\033[1;30m\nsorting vector...\033[1;32m" << std::endl;
-	sort(cont1);
+	merge_insert_sort(cont1, 0, cont1.size() - 1);
 	gettimeofday(&tv2, NULL);
 	double time1 = (double) (tv2.tv_sec - tv1.tv_sec);
 	time1 = (double) ((time1 * 1000000) + (tv2.tv_usec - tv1.tv_usec));
@@ -79,24 +79,23 @@ int	main(int ac, char**av)
 	for (unsigned long i = 0; i < cont1.size(); i++)
 		std::cout << cont1[i] << " ";
 	std::cout << std::endl;
+	std::cout << "\033[1;32m\nTime to process a range of " << cont1.size() << " elements";
+	std::cout << " with std::vector :  \033[1;33m" << time1 << "\033[1;32m us" << std::endl;
 
 	gettimeofday(&tv1, NULL);
 	std::deque<int> cont2 = fillCont<std::deque<int> >(input);
 	std::cout << "\033[1;30m\nsorting deque...\033[1;32m" << std::endl;
-	sort(cont2);
+	merge_insert_sort(cont2, 0, cont2.size() - 1);
 	gettimeofday(&tv2, NULL);
 	double time2 = (double) (tv2.tv_sec - tv1.tv_sec);
 	time2 = (double) ((time2 * 1000000) + (tv2.tv_usec - tv1.tv_usec));
 
-	std::cout << "\033[1;32mAfter (\033[1;31mstd::list\033[1;32m)   :  \033[1;33m";
+	std::cout << "\033[1;32mAfter (\033[1;31mstd::deque\033[1;32m)   :  \033[1;33m";
 	std::deque<int>::iterator it = cont2.begin();
 	for (unsigned long i = 0; i < cont2.size(); i++)
 		std::cout << *(it++) << " ";
 	std::cout << std::endl;
-
-	std::cout << "\033[1;32m\nTime to process a range of " << cont1.size() << " elements";
-	std::cout << " with std::vector :  \033[1;33m" << time1 << "\033[1;32m us" << std::endl;
-	std::cout << "\033[1;32mTime to process a range of " << cont2.size() << " elements";
+	std::cout << "\033[1;32m\nTime to process a range of " << cont2.size() << " elements";
 	std::cout << " with std::deque   :  \033[1;33m" << time2 << "\033[1;32m us\033[0m" << std::endl;
 
 	return 0;
