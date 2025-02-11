41c5e043d55d8f9d0b1767f75ebac9821ae3d144
Kilyan Poilly <kpoilly@paul-f4Br9s2.clusters.42paris.fr>
2025-02-11 16:08:22 +0100

    Python01:ex01

--- Python01/ex01/array2D.py
+++ Python01/ex01/array2D.py
@@ -17,7 +17,7 @@ def slice_me(family: list, start: int, end: int) -> list:
 
     if not isinstance(family, (list, np.ndarray)):
         raise TypeError("family should be list or numpy arrays.")
-    
+
     if not isinstance(start, int) or not isinstance(end, int):
         raise TypeError("start and end should be ints.")
 
