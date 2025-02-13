2fadd7aa16bbe3e3588dd41c8ec6f1e75a9e3fde
root <root@DESKTOP-I8GCES8>
2025-02-13 17:54:10 +0100

    moved to new repo

--- Python00/ex01/format_ft_time.py
+++
@@ -1,5 +0,0 @@
-import time
-import datetime
-
-date = time.time()
-print("Seconds since January 1, 1970: ", date, " or ", '{:.2e}'.format(date), " in scientific notation\n", datetime.datetime.fromtimestamp(time.time()).strftime('%b %d %Y'))
